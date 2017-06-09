bba30a7894e57ce1617e6ea8f1c0996583d8049d
Chris Shaffer <chris@methodbox.io>
2017-06-08 22:34:40 -0500

    continuing refactor, server & common error recog simplified

--- src/App.vue
+++ src/App.vue
@@ -49,22 +49,17 @@
         </div>
         <div class="row" v-if='common'>
           <div class="col s12">
-            <common-errors v-bind:commonErrors='commonErrors'></common-errors>
+            <common-error v-bind:commonErrors='commonErrors'></common-error>
           </div>
         </div>
-        <div class="row" v-if='serverErr'>
+        <div class="row" v-if='server'>
           <div class="col s12">
-            <server-errors v-bind:serverErrs='serverErrs'></server-errors>
+            <server-error v-bind:serverErrors='serverErrors'></server-error>
           </div>
         </div>
         <div class="row" v-if='unknown'>
           <div class="col s12">
             <unknown></unknown>
-          </div>
-        </div>
-        <div class="row" v-if='testView'>
-          <div class="col s12">
-            <test v-bind:test='test'></test>
           </div>
         </div>
       </div>
@@ -75,19 +70,17 @@
 <script>
 import Wordpress from './components/Wordpress'
 import Unknown from './components/Unknown'
-import commonErrors from './components/commonErrors'
-import serverErrors from './components/serverErrors'
+import CommonError from './components/CommonError'
+import ServerError from './components/ServerError'
 import Instructions from './components/Instructions'
-import Test from './components/Test'
 export default {
   name: 'app',
   components: {
     Wordpress,
-    commonErrors,
+    CommonError,
+    ServerError,
     Unknown,
-    serverErrors,
-    Instructions,
-    Test
+    Instructions
   },
   data () {
     return {
@@ -95,102 +88,88 @@ export default {
         //  conditional for displaying Instructions on-click
         instr: false
       },
+      //  A short description of the error - WP & Server error templates are passed this value
+      errorName: '',
       //  booleans to determine which template is rendered
       wordpress: false,
       common: false,
-      testView: false,
-      test: false,
-      //  A short description of the error - WP & Server error templates are passed this value
-      errorName: '',
-      //  WP specific errors template
+      server: false,
+      //  unrecognized errors template
+      unknown: false,
+      //  WP errors template props
       wp: {
         themePlug: ''
       },
-      //  common errors template
+      //  common errors template props
       commonErrors: {
         notFound: false,
         forbidden: false
       },
-      //  unrecognized errors template
-      unknown: false,
-      //  server errors template
-      serverErr: false,
-      feedbackForm: false,
-      serverErrs: {
-        errorType: '',
-        description: '',
-        scriptLink: '',
-        helpLink: '',
-        helpLinkBool: false,
-        solutionOne: '',
-        solutionTwo: '',
-        solutionThree: '',
-        solutionFour: '',
-        solutionFive: '',
-        solutionSix: ''
+      //  server errors template props
+      serverErrors: {
+        tmp: false,
+        internal: false,
+        fiveHundred: false
       }
-
     }
   },
   methods: {
-    instructionsNav () {
-      if (this.nav.instr === false) {
-        this.nav.instr = true
-      } else {
-        this.nav.instr = false
-      }
-    },
     resetForm () {
       this.wordpress = false
       this.common = false
       this.commonErrors.forbidden = false
       this.commonErrors.notFound = false
     },
-    errorFormSubmit (event) {
+    searchReset () {
+      document.getElementById('error-field').value = ''
+    },
+    errorFormSubmit () {
       this.resetForm()
       let errorText = document.getElementById('error-field')
-      let parseError = errorText.value.split(' ')
-      let wpParseError = errorText.value.split('/')
+      let parseError = errorText.value.split(' ')// parse the error
+      let wpParseError = errorText.value.split('/')// parse the path in the error - looking for WP values
+      //  filter to determine the error type
       let wpFilter = wpParseError.filter(function (err) {
+        //  filter out error-related values
         if (err === 'themes' || err === 'plugins' || err === 'wp-admin') {
-          return err
+          return err//  send them to an array
         }
       })
+
       let commonFilter = parseError.filter(function (err) {
         if (err === 'Forbidden' || err === 'Not' || err === 'Found') {
           return err
         }
       })
+      //  filter out server error related values
       let serverFilter = parseError.filter(function (err) {
-        console.log(err)
+        if (err === 'session_start():' || err === 'Unknown:') {
+          return err
+        }
       })
-      //  sends to wpEvaluate to determine if the error is WP if wpFilter is not empty
-      if (wpFilter.length !== 0) {
-        this.wpEvaluate(wpFilter)
-        this.errorName = errorText.value.substring(0, 12)
+      //  evaluate the error down to specific template type
+      if (wpFilter.length !== 0) { //  check if the err array has a value
+        this.wpEvaluate(wpFilter) // fire a sub event to handle rendering the proper template
+        this.errorName = errorText.value.substring(0, 12) // grab parse of error value and set as desc
       }
-      //  fires common event if commonFilter is not empty
       if (commonFilter.length !== 0) {
         this.commonEvent(commonFilter)
       }
-      //  fires server error event if serverFilter is not empty
       if (serverFilter.length !== 0) {
-        console.log('test')
+        this.serverEvent(serverFilter)
       }
     },
+    //  template conditional rendering methods
     wpEvaluate (errorPath) {
       let wpError = errorPath[0]
-      //  gets the path of the file calling the error and parses out theme or plugin
+      //  gets the path of the file calling the error and parses out 'themes' or 'plugins'
       if (wpError === 'themes') {
-        console.log('Theme')
-        this.wordpress = true
-        this.wp.themePlug = 'Theme'
+        this.wordpress = true// sets the conditional to render the proper template
+        this.wp.themePlug = 'Theme' //  sets the theme or plugin value for WP template
       } else if (wpError === 'plugins') {
-        console.log('Plugins')
         this.wordpress = true
         this.wp.themePlug = 'Plugin'
-      } else if (wpError === 'wp-admin') {
-        console.log('Either')
+      } else if (wpError === 'wp-admin' || wpError === 'wp-includes') {
         this.wordpress = true
         this.wp.themePlug = 'Theme or Plugin'
       }
@@ -203,67 +182,23 @@ export default {
         this.commonErrors.notFound = true
       }
     },
-    searchReset () {
-      document.getElementById('error-field').value = ''
-    },
-    testEvent () {
-      this.test = true
-      this.testView = true
-    },
-    fiveHundredEvent () {
-      this.resetForm()
-      this.serverErrs.errorType = 'Database connection'
-      this.serverErrs.description = 'Something has prevented the application from connecting to the database. This can be an application or server issue.'
-      this.serverErrs.scriptLink = 'https://confluence.godaddy.com/download/attachments/20748053/mysql.php?version=3&modificationDate=1434049926000&api=v2&download=true'
-      this.serverErrs.helpLinkBool = true
-      this.serverErrs.helpLink = 'https://www.godaddy.com/help/what-are-the-connection-strings-for-my-applications-database-5199'
-      this.serverErrs.solutionOne = 'First make sure the DB exists. Ask the customer for the DB name. Or find it in the config file: '
-      this.serverErrs.solutionTwo = 'If this is cPanel or Plesk, make sure there is a user assigned to the database.'
-      this.serverErrs.solutionThree = 'Try to access the DB via PHPMyAdmin - if this is accessible, then the app should be able to connect, too.'
-      this.serverErrs.solutionFour = 'Use the script from Required Scripts column to test the connection w/your own user/password.'
-      this.serverErrs.solutionFive = 'If neither PHPMyAdmin or your test script can connect to the database, take this issue to a Tech Lead'
-      this.serverErrs.solutionSix = 'If your test script worked and this IS a WordPress site offer WPPS.'
-      this.serverErr = true
-    },
-    iseEvent () {
-      this.resetForm()
-      this.serverErrs.errorType = '500 Internal Server Error'
-      this.serverErrs.description = 'This error is generic and means the application had a problem, but may be related to a problem on the server.'
-      this.serverErrs.link = 'http://localhost:3000'
-      this.serverErrs.helpLinkBool = false
-      this.serverErrs.solutionOne = 'If this is a PHP-based site (check for index.php), trying running a test script. You can download one from the Required Script column.'
-      this.serverErrs.solutionTwo = 'If your test script also produces a 500 or ISE error, try disabling the .htaccess file. If the error still occurs, take this to a Tech Lead.'
-      this.serverErrs.solutionThree = 'If your test script worked and this is not a WordPress site, the customer or their developer will need to address the issue on their own.'
-      this.serverErrs.solutionFour = 'If your test script worked and this IS a WordPress site, this issue is caused by WP or a theme or plugin. Offer WPPS.'
-      this.serverErrs.solutionFive = ''
-      this.serverErrs.solutionSix = ''
-      this.serverErr = true
-    },
-    tmpEvent (tmpPath) {
-      this.resetForm()
-      this.serverErrs.errorType = 'tmp folder or session issue'
-      this.serverErrs.description = 'This error is usually a problem on the server.  It is likely this will need to go to a Tech Lead'
-      this.serverErrs.link = 'https://confluence.godaddy.com/download/attachments/20748053/phpinfo.php?version=2&modificationDate=1493746207000&api=v2&download=true'
-      this.serverErrs.helpLinkBool = false
-      this.serverErrs.solutionOne = 'All hosting: check for .ini files in root, home, public_html or httdocs that define /tmp as a specific location'
-      this.serverErrs.solutionTwo = 'Disable any .ini file and have the customer test again, continue to the next step if not resolved'
-      this.serverErrs.solutionThree = 'First download "phpinfo.php" from the Required Scripts column and upload it to the site root directory'
-      this.serverErrs.solutionFour = 'cPanel: Visit domain.com/phpinfo.php. Verify session.save_path is set to "/tmp". If not, go to a TL'
-      this.serverErrs.solutionFive = 'Plesk: Visit domain.com/phpinfo.php. Verify session.save_path is set to "C:\\Windows\\Temp" If not, got to a TL'
-      this.serverErrs.solutionSix = 'For 4gh most likely the /tmp folder is full or missing. Take this issue to a TL'
-      if (tmpPath === 'session') {
-        this.serverErr = true
-      } else {
-        for (let t = 0; t < tmpPath.length; t++) {
-          if (tmpPath[t] === 'tmp') {
-            this.serverErr = true
-          }
-        }
+    serverEvent (serverErrorType) {
+      let serverError = serverErrorType[0]
+      if (serverError === 'session_start():' || serverError === 'Unknown:') {
+        this.server = true
+        this.serverErrors.tmp = true
       }
     },
     unknownEvent () {
       this.resetForm()
       this.unknown = true
+    },
+    instructionsNav () {
+      if (this.nav.instr === false) {
+        this.nav.instr = true
+      } else {
+        this.nav.instr = false
+      }
     }
   }
 }
